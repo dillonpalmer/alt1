f55e579d6e08a5892d89a44226de2426d5398968
Dillon Palmer <44123391+dillonpalmer@users.noreply.github.com>
2019-11-20 11:51:21 +0000

    Create readingLatestData.py

---
+++ readingLatestData.py
@@ -0,0 +1,22 @@
+from time import sleep
+from firebase import firebase
+firebase = firebase.FirebaseApplication('https://alt1-8f179.firebaseio.com/MyTemperatures/', None)
+
+#reads the full database
+result = firebase.get('/MyTemperatures', None)
+print(result)
+print("  ")
+
+#reads the latest piece of data
+names = []
+
+for name in result:
+    names.append(name)
+    
+result1 = (str(result))
+print("Latest data recorded...")
+sleep(1)
+print(" ")
+
+print("Temperature: ", result[names[-1]]['Temperature'])
+
